e8fab1e79b3a020fad0254ad72bf00f506b5afbb
iia tronics <iia.yatskova@gmail.com>
2025-11-30 00:35:53 +0200

    added tests for palindrome func(0/2 tests passed)

--- test/unit_tests_borderline_cases.cpp
+++ test/unit_tests_borderline_cases.cpp
@@ -6,6 +6,7 @@ using testing::internal::GetCapturedStdout;
 
 //TEST for extreme cases, with exeptions
 
+//reverse 
 TEST(string_func_extreme_test, reverse_string_blank)
 {
     string input = "";
@@ -20,4 +21,28 @@ TEST(string_func_extreme_test, reverse_string_spaces)
 
     reverse_string(input);
     ASSERT_EQ("   ", input);
+}
+
+//palindrome
+
+TEST(string_func_extreme_test, polindrome_check_with_spaces)
+{
+    string input = "a bc c ba";
+
+    string expected = "ab c cb a";
+
+    ASSERT_TRUE(is_polindrome(input));
+
+    ASSERT_EQ(expected, input) << "string is changed to its polindrome\n";
+}
+
+TEST(string_func_extreme_test, polindrome_check_with_capital)
+{
+    string input = "mADam";
+
+    string expected = "maDAm";
+
+    ASSERT_TRUE(is_polindrome(input));
+
+    ASSERT_EQ(expected, input) << "string is changed to its polindrome\n";
 }
